a61dbdcc8a4e4ae90309985965c47d91e81df344
yancl <kmoving@gmail.com>
2013-04-24 14:08:59 +0800

    fix import bug

--- collectd/__init__.py
+++ collectd/__init__.py
@@ -1,5 +1,6 @@
 from protocol.genpy.collectd.ttypes import Point, Event, TimeSlice, ETimeSlicePointType
 from utils import now
 from client import Stats
+from aggregator import Aggregator
 
 __all__ = ['Aggregator', 'Stats', 'Event', 'TimeSlice', 'Point', 'ETimeSlicePointType', 'now']
