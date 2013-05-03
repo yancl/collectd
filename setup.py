bfcc6bfe1309ab8f1e0e0ce7770cbba874986b0d
yancl <kmoving@gmail.com>
2013-05-03 11:46:03 +0800

    add publish arg to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
-
+import os
+import sys
 from distutils.core import setup
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r internal')
+    sys.exit()
+
 setup(
-        name = "collectd",
-        version = "0.10",
+        name="collectd",
+        version="0.10",
         description="python stats service",
-        maintainer="yancl",
-        maintainer_email="kmoving@gmail.com",
+        long_description=open("README.md").read(),
+        author="yancl",
+        author_email="kmoving@gmail.com",
+        url='https://github.com/yancl/collectd',
+        classifiers=[
+            'Programming Language :: Python',
+        ],
+        platforms='Linux',
+        license='MIT License',
+        zip_safe=False,
+        install_requires=[
+            'setuptools',
+        ],
+        tests_require=[
+            'nose',
+        ],
         packages=['collectd', 'collectd.protocol', 'collectd.protocol.genpy', 'collectd.protocol.genpy.collectd']
 )
